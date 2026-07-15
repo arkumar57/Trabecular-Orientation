82778d2a80a35f2be8e47146e28134a8c8523dda
arkumar57 <arkumar57@outlook.com>
2026-07-14 19:01:51 -0700

    From Scratch

---
+++ Orientation.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+csv_path = "0.21_0.26_projection.csv"
+Tiffs_folder_path = "anisotropy_images/*.tiff"
+csv_df = pd.read_csv(csv_path, sep = ";")
+
+
+# Load Position and Vectors from csv file
+
+positions = csv_df[["PosX", "PosY", "PosZ"]].to_numpy(float)
+vectors = csv_df[["VectX", "VectY", "VectZ"]].to_numpy(float)
+
+
+vectors = vectors / np.linalg.norm(vectors, axis = 1, keepdims = True)
+
+
+resulting_matrix = vectors.T @ vectors
+
+eigen_values, eigen_vectors = np.linalg.eigh(resulting_matrix)
+
+principal_direction = eigen_vectors[:, -1]
+
+angle_from_x_axis = np.degrees(np.arccos(abs(d[0])))
+
+angle_from_y_axis = np.degrees(np.arccos(abs(d[1])))
+
+angle_from_z_axis = np.degrees(np.arccos(abs(d[2])))
+
+print("Dominant direction:", np.round(d, 3))
+print("Angle from Z-axis:", round(angle_from_z_axis, 1), "deg")
+print("Angle from X-axis:", round(angle_from_x_axis, 1), "deg")
+print("Angle from Y-axis:", round(angle_from_y_axis, 1), "deg")
